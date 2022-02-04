bc1836249fb75d035bcd70c201e668a05c32838e
trapintrovert <mcneo062@gmail.com>
2022-02-04 11:37:04 +0100

    integrating MetaMask

--- defi-staking-app-starter/src/components/App.js
+++ defi-staking-app-starter/src/components/App.js
@@ -5,17 +5,39 @@ import Web3 from 'web3'
 
 class App extends Component{
 
-    async Web3(){
+    async UNSAFE_componentWillMount(){
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+    async loadWeb3(){
         if(window.ethereum){
             window.web3 = new Web3(window.ethereum)
             await window.ethereum.enable()
+        }else if(window.web3){
+            window.web3 = new Web3(window.web3.currentProvider)
+        }else{
+            window.alert('No ethereum browser detected: You can check out MetaMask')
         }
+    }
+
+    async loadBlockchainData(){
+        const web3 = window.web3
+        const account = await web3.eth.getAccounts()
+        console.log(account)
     }
 
     constructor(props){
         super(props)
         this.state = {
-            account: '0x0'
+            account: '0x0',
+            tether: {},
+            rwd: {},
+            decentralBank: {},
+            tetherBalance: '0',
+            rwdBalance:'0',
+            stakingBalance: '0',
+            loading: true
         }
     }
 
